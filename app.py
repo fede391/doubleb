cdca83266c1537d0239dbb3e49c214aad323d9e6
Federico Capellini <fede.capellini@gmail.com>
2026-04-11 16:08:53 +0200

    fix app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="DoubleB", page_icon="👶")
+
 st.markdown(
     """
     <style>
@@ -12,14 +16,11 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-import streamlit as st
 from ui.sections import (
     render_kpi_header,
     render_quick_log,
     render_event_timeline,
 )
-
-st.set_page_config(page_title="DoubleB", page_icon="👶")
 
 st.title("DoubleB 👶")
 
