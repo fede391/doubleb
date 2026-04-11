ac9ade93eb73f27178ed17c66ab83ce4b370d3ba
Federico Capellini <fede.capellini@gmail.com>
2026-04-11 10:18:14 +0200

    Setup for hosting on Stramlit Community CLoud

--- db.py
+++ db.py
@@ -1,13 +1,14 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from supabase import create_client, Client
 
 load_dotenv()
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+url = st.secrets.get("SUPABASE_URL") or os.getenv("SUPABASE_URL")
+key = st.secrets.get("SUPABASE_KEY") or os.getenv("SUPABASE_KEY")
 
 if not url or not key:
-    raise ValueError("Missing SUPABASE_URL or SUPABASE_KEY in .env")
+    raise ValueError("Missing SUPABASE_URL or SUPABASE_KEY")
 
 supabase: Client = create_client(url, key)
