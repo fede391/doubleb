92f9ab9360c7f0298e45deae8ee34e83a53cf7f4
Federico Capellini <fede.capellini@gmail.com>
2026-04-11 15:58:22 +0200

    fix spacing on top

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+st.markdown(
+    """
+    <style>
+    .block-container {
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+        padding-left: 0.8rem;
+        padding-right: 0.8rem;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 import streamlit as st
 from ui.sections import (
     render_kpi_header,
